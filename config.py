afb52b98505d0425c9dc1647aec71e101acbfbc5
Fawwaz Ijlal Muqsith <reinjin4@gmail.com>
2024-10-24 12:05:51 +0700

    membuat konfigurasi load_dotenv() pada config.py tidak aktif untuk melakukan build

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-# Muat file .env
-load_dotenv()
+# Muat file .env  , aktifkan jika menjalankan di lokal
+#load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey') 
