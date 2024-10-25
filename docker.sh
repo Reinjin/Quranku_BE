06e037959084792e335800312784760c0adb05ef
Fawwaz Ijlal Muqsith <reinjin4@gmail.com>
2024-10-25 23:55:17 +0700

    mengurutkan perintah di docker.sh

--- docker.sh
+++ docker.sh
@@ -6,12 +6,12 @@ docker container create --name mysql -e MYSQL_ROOT_PASSWORD=178292219 -e MYSQL_D
 # start mysql container
 docker container start mysql
 
-### PERINTAH UNTUK MENJALANKAN DOCKER-COMPOSE.YAML ###
-docker-compose up -d
-docker-compose down 
-
 ### PERINTAH UNTUK BUILD APP ###
 docker build -t reinjin/quranku_app .
 
 ### PEINTAH UNTUK MEMBUAT DAN MENJALANKAN CONTAINER LOKAL ###
 docker container create --name quranku_app -e SECRET_KEY=your_secret_key -e JWT_SECRET_KEY=your_JWT_secretkey -e DATABASE_URL=your_database_connection_string -p 5003:5003 reinjin/quranku_app:latest
+
+### PERINTAH UNTUK MENJALANKAN DOCKER-COMPOSE.YAML ###
+docker-compose up -d
+docker-compose down 
